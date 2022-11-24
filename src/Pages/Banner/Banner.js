61683388bd3bb53e92c49c6a3a1c24dfbbb1877d
Md. Saiful Islam <saiful9122113@gmail.com>
2022-11-24 12:51:34 +0600

    slider added here

---
+++ src/Pages/Banner/Banner.js
@@ -0,0 +1,84 @@
+import React from "react";
+import image1 from "../../Assets/images/banner/b1.jpg";
+import image2 from "../../Assets/images/banner/b2.jpg";
+import image3 from "../../Assets/images/banner/b3.jpg";
+import image4 from "../../Assets/images/banner/b4.jpg";
+import image5 from "../../Assets/images/banner/b5.jpg";
+import image6 from "../../Assets/images/banner/b6.jpg";
+
+const Banner = () => {
+  return (
+    <div className="flex justify-center">
+      <div className="carousel  w-2/3">
+        <div id="slide1" className="carousel-item relative w-full">
+          <img src={image1} alt="" className="w-full" />
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide4" className="btn btn-circle">
+              ❮
+            </a>
+            <a href="#slide2" className="btn btn-circle">
+              ❯
+            </a>
+          </div>
+        </div>
+        <div id="slide2" className="carousel-item relative w-full">
+          <img src={image2} alt="" className="w-full" />
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide1" className="btn btn-circle">
+              ❮
+            </a>
+            <a href="#slide3" className="btn btn-circle">
+              ❯
+            </a>
+          </div>
+        </div>
+        <div id="slide3" className="carousel-item relative w-full">
+          <img src={image3} alt="" className="w-full" />
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide2" className="btn btn-circle">
+              ❮
+            </a>
+            <a href="#slide4" className="btn btn-circle">
+              ❯
+            </a>
+          </div>
+        </div>
+        <div id="slide4" className="carousel-item relative w-full">
+          <img src={image4} alt="" className=" w-full" />
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide3" className="btn btn-circle">
+              ❮
+            </a>
+            <a href="#slide5" className="btn btn-circle">
+              ❯
+            </a>
+          </div>
+        </div>
+        <div id="slide5" className="carousel-item relative w-full">
+          <img src={image5} alt="" className="w-full" />
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide4" className="btn btn-circle">
+              ❮
+            </a>
+            <a href="#slide6" className="btn btn-circle">
+              ❯
+            </a>
+          </div>
+        </div>
+        <div id="slide6" className="carousel-item relative w-full">
+          <img src={image6} alt="" className="w-full" />
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide5" className="btn btn-circle">
+              ❮
+            </a>
+            <a href="#slide1" className="btn btn-circle">
+              ❯
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
--- src/Pages/Shared/Navbar/Navbar.js
+++ src/Pages/Shared/Navbar/Navbar.js
@@ -71,7 +71,9 @@ const Navbar = () => {
             {menuItems}
           </ul>
         </div>
-        <Link to="/" className="btn btn-ghost normal-case text-xl">ReSell</Link>
+        <Link to="/" className="btn btn-ghost normal-case text-xl">
+            ReSell
+        </Link>
       </div>
       <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal p-0">
